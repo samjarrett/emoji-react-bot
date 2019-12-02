4fbe5df9582cf37ca4d26ff6fd90ce6a16bc86d2
Sam Jarrett <samstah@gmail.com>
2019-12-02 23:28:57 +1100

    black producer_bot main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import producer_bot
 
 BOT = producer_bot.get_bot(os.environ["SLACK_API_TOKEN"])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     BOT.start()
--- producer_bot/bot.py
+++ producer_bot/bot.py
@@ -6,19 +6,19 @@ PHRASES = [
     ("check a box", "ballot_box_with_check"),
 ]
 
-@slack.RTMClient.run_on(event='message')
+
+@slack.RTMClient.run_on(event="message")
 def on_message(**payload):
-    data = payload['data']
-    web_client = payload['web_client']
-    text = data.get('text', '')
+    data = payload["data"]
+    web_client = payload["web_client"]
+    text = data.get("text", "")
 
     for phrase, emoji in PHRASES:
         if phrase in text:
             web_client.reactions_add(
-                channel=data['channel'],
-                timestamp=data['ts'],
-                name=emoji
+                channel=data["channel"], timestamp=data["ts"], name=emoji
             )
+
 
 def get_bot(token: str) -> slack.RTMClient:
     ssl_context = ssl.create_default_context()
