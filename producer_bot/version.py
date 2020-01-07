bc0d60195ac3efd55968566788e2d661614b7508
Sam Jarrett <samstah@gmail.com>
2020-01-07 21:17:32 +1100

    Fix pylint issue in version.py

--- producer_bot/version.py
+++ producer_bot/version.py
@@ -7,7 +7,7 @@ def get_version():
     directory = os.path.dirname(os.path.realpath(__file__))
     version_filename = f"{directory}/../VERSION"
 
-    with open(version_filename, "r") as f:
-        version = f.read().rstrip("\n")
+    with open(version_filename, "r") as version_file:
+        version = version_file.read().rstrip("\n")
 
     return version
