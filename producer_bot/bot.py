35080d104cd196409cf49230175147a4030c7967
David Bell <david@bell.id.au>
2022-04-22 15:35:31 +1000

    started adding tracing

--- producer_bot/bot.py
+++ producer_bot/bot.py
@@ -5,6 +5,12 @@ from typing import Dict
 import slack_sdk
 from slack_sdk.rtm import RTMClient
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 from . import corrector, dice_roller, triggered_reactions
 from .parrot import Parrot
 from .slack_helper import (
@@ -28,6 +34,16 @@ EMOJI_VERBIAGE = {
 }
 
 PARROT = Parrot(ADMIN_DEBUG_CHANNEL)
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+HONEYCOMB_TOKEN = os.environ.get("HONEYCOMB_TOKEN")
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="https://api.honeycomb.io/v1/traces", headers={"x-honeycomb-team": HONEYCOMB_TOKEN}))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+RequestsInstrumentor().instrument()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -57,95 +73,119 @@ def on_goodbye(**kwargs):  # pylint: disable=unused-argument
 def on_message(
     data: Dict, web_client: slack_sdk.WebClient, **kwargs
 ):  # pylint: disable=unused-argument
-    # handle different structure of edited messages correctly
-    if data.get("subtype") == "message_replied":
-        logging.debug("Skipping event as it's a message_replied event")
-        return
+    with tracer.start_as_current_span("on_message"):
+        span = trace.get_current_span()
+        # handle different structure of edited messages correctly
+        if data.get("subtype") == "message_replied":
+            span.set_attribute("app.message.type", "message_replied")
+            logging.debug("Skipping event as it's a message_replied event")
+            return
 
-    text = data.get("text", "").lower()
-    channel = data["channel"]
-    timestamp = data["ts"]
-    user = data.get("user", "")
+        text = data.get("text", "").lower()
+        channel = data["channel"]
+        timestamp = data["ts"]
+        user = data.get("user", "")
 
-    try:
-        triggered_reactions.handle_message(channel, timestamp, text, web_client)
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+        span.set_attribute("app.channel", data["channel"])
+        span.set_attribute("app.timestamp", data["ts"])
+        span.set_attribute("app.user", data.get("user", ""))
 
-    if "dice" in text:
         try:
-            dice_roller.roll(channel, timestamp, web_client)
+            triggered_reactions.handle_message(channel, timestamp, text, web_client)
         except slack_sdk.errors.SlackApiError as exception:
             logging.error(exception)
 
-    try:
-        corrector.trigger(channel, timestamp, user, text, web_client)
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+        if "dice" in text:
+            try:
+                dice_roller.roll(channel, timestamp, web_client)
+            except slack_sdk.errors.SlackApiError as exception:
+                logging.error(exception)
 
-    try:
-        if "bot_id" not in data:  # don't trigger on bots
-            bot_user_id = get_bot_user_id(web_client).lower()
-            if text.startswith(f"<@{bot_user_id}>") or is_channel_im(
-                channel, web_client
-            ):
-                PARROT.on_app_mention(
-                    web_client, text, channel, data.get("thread_ts", None), user
-                )
-            PARROT.on_message(web_client, text, channel, timestamp, user)
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+        try:
+            corrector.trigger(channel, timestamp, user, text, web_client)
+        except slack_sdk.errors.SlackApiError as exception:
+            logging.error(exception)
+
+        try:
+            if "bot_id" not in data:  # don't trigger on bots
+                bot_user_id = get_bot_user_id(web_client).lower()
+                if text.startswith(f"<@{bot_user_id}>") or is_channel_im(
+                    channel, web_client
+                ):
+                    PARROT.on_app_mention(
+                        web_client, text, channel, data.get("thread_ts", None), user
+                    )
+                PARROT.on_message(web_client, text, channel, timestamp, user)
+        except slack_sdk.errors.SlackApiError as exception:
+            logging.error(exception)
 
 
 @RTMClient.run_on(event="reaction_added")
 def on_reaction_added(
     data: Dict, web_client: slack_sdk.WebClient, **kwargs
 ):  # pylint: disable=unused-argument
-    if data["reaction"] == BACKTRACK_EMOJI:
-        reactions_item, item_type = event_item_to_reactions_api(data["item"])
-        bot_id = get_bot_user_id(web_client)
+    with tracer.start_as_current_span("on_reaction_added"):
+        span = trace.get_current_span()
+        span.set_attribute("app.reaction", data["reaction"])
+        span.set_attribute("app.channel", data["item"]["channel"])
+        span.set_attribute("app.timestamp", data["item"]["ts"])
+        span.set_attribute("app.user", data.get("user", ""))
 
-        bot_reactions = get_bot_reactions(web_client, bot_id, item_type, reactions_item)
+        if data["reaction"] == BACKTRACK_EMOJI:
+            reactions_item, item_type = event_item_to_reactions_api(data["item"])
+            bot_id = get_bot_user_id(web_client)
 
-        for reaction in bot_reactions:
-            web_client.reactions_remove(name=reaction.get("name"), **reactions_item)
+            bot_reactions = get_bot_reactions(web_client, bot_id, item_type, reactions_item)
 
-    try:
-        PARROT.on_reaction_added(
-            web_client,
-            data["reaction"],
-            data["item"]["channel"],
-            data["item"]["ts"],
-            data["user"],
-        )
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+            for reaction in bot_reactions:
+                web_client.reactions_remove(name=reaction.get("name"), **reactions_item)
+
+        try:
+            PARROT.on_reaction_added(
+                web_client,
+                data["reaction"],
+                data["item"]["channel"],
+                data["item"]["ts"],
+                data["user"],
+            )
+        except slack_sdk.errors.SlackApiError as exception:
+            logging.error(exception)
 
 
 @RTMClient.run_on(event="reaction_removed")
 def on_reaction_removed(
     data: Dict, web_client: slack_sdk.WebClient, **kwargs
 ):  # pylint: disable=unused-argument
-    try:
-        PARROT.on_reaction_removed(
-            web_client,
-            data["reaction"],
-            data["item"]["channel"],
-            data["item"]["ts"],
-            data["user"],
-        )
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+    with tracer.start_as_current_span("on_reaction_removed"):
+        span = trace.get_current_span()
+        span.set_attribute("app.reaction", data["reaction"])
+        span.set_attribute("app.channel", data["item"]["channel"])
+        span.set_attribute("app.timestamp", data["item"]["ts"])
+        span.set_attribute("app.user", data.get("user", ""))
+        try:
+            PARROT.on_reaction_removed(
+                web_client,
+                data["reaction"],
+                data["item"]["channel"],
+                data["item"]["ts"],
+                data["user"],
+            )
+        except slack_sdk.errors.SlackApiError as exception:
+            logging.error(exception)
 
 
 @RTMClient.run_on(event="user_typing")
 async def on_user_typing(
     data: Dict, rtm_client: RTMClient, **kwargs
 ):  # pylint: disable=unused-argument
-    try:
-        await PARROT.on_user_typing(rtm_client, data["channel"], data["user"])
-    except slack_sdk.errors.SlackApiError as exception:
-        logging.error(exception)
+    with tracer.start_as_current_span("on_user_typing"):
+        span = trace.get_current_span()
+        span.set_attribute("app.channel", data["channel"])
+        span.set_attribute("app.user", data.get("user", ""))
+        try:
+            await PARROT.on_user_typing(rtm_client, data["channel"], data["user"])
+        except slack_sdk.errors.SlackApiError as exception:
+            logging.error(exception)
 
 
 @RTMClient.run_on(event="emoji_changed")
