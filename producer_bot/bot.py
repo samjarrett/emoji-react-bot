b706ece59727a53602c808f7227756173fc9e01a
Sam Jarrett <samstah@gmail.com>
2022-08-01 16:59:45 +1000

    Also fix the on-hello message

--- producer_bot/bot.py
+++ producer_bot/bot.py
@@ -34,8 +34,6 @@ EMOJI_VERBIAGE = {
     "remove": "removed",
 }
 
-FIRST_CONNECT = True
-
 PARROT = Parrot(ADMIN_DEBUG_CHANNEL)
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -64,11 +62,15 @@ def on_hello(
 ):  # pylint: disable=unused-argument
     logging.info("Bot connected to the server")
 
-    web_client.chat_postMessage(
-        channel=ADMIN_DEBUG_CHANNEL,
-        text=f":hello-my-name-is: Bot version {get_version()} connected on {get_instance_hash()} (first connect: {FIRST_CONNECT})",
-    )
-    FIRST_CONNECT = False
+    if on_hello.FIRST_CONNECT:
+        on_hello.FIRST_CONNECT = False
+        web_client.chat_postMessage(
+            channel=ADMIN_DEBUG_CHANNEL,
+            text=f":hello-my-name-is: Bot version {get_version()} connected on {get_instance_hash()}",
+        )
+
+
+on_hello.FIRST_CONNECT = True
 
 
 @RTMClient.run_on(event="goodbye")
